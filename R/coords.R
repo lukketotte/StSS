5a670c581993fed4978ab66eb2bed8a56190d92c
Lukas <lukas.arnroth@gmail.com>
2025-05-07 13:50:35 +0200

    adding dataset

---
+++ R/coords.R
@@ -0,0 +1,9 @@
+#' Coordinates from small tissue section for simulation
+#'
+#'
+#' @format A dataframe with 341 rows and 2 columns
+#' \describe{
+#' \item{x,y}{Spatial coordinates}
+#'}
+#'
+"coords"
